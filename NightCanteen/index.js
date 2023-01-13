ab4b1c769c910d42104c6ecc81f341207fafbbd1
FRANKENSTEIN0211 <98984204+FRANKENSTEIN0211@users.noreply.github.com>
2023-01-14 01:55:44 +0530

    sin in complete

---
+++ NightCanteen/index.js
@@ -0,0 +1,17 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").text(profile.getImageUrl());
+    $(".data").css("display","none");
+    $(".g-signin2").css("display","none");
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert("You have been signed out succeddfully");
+      $(".g-signin2").css("display","block");
+      $(".data").css("display","none");
+    });
+  }
